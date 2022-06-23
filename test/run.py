9514967a6cb9f3e5a0ad9e260a2b086aaf4fbc73
Russell Jarvis <russelljjarvis@protonmail.com>
2022-06-23 14:44:46 +1000

    update all

--- test/run.py
+++ test/run.py
@@ -10,9 +10,9 @@ VU.add_verification_components()
 SRC_PATH = Path(__file__).parent / "src"
 
 VU.add_library("lib").add_source_files(
-    [SRC_PATH / "*.v", SRC_PATH / "test" / "*.v"]
+    [SRC_PATH / "*.vs", SRC_PATH / "test" / "*.vs"]
 )
-print([SRC_PATH / "*.v", SRC_PATH / "test" / "*.v"])
+print([SRC_PATH / "*.vs", SRC_PATH / "test" / "*.vs"])
 
 VU.main()
 print("did this work?")
